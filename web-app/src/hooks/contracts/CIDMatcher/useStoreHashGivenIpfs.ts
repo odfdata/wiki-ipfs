f5806351abaa538c0a80f9af162ff6953b372097
Federico Castelli <fc@oracleofde.fi>
2022-11-17 16:27:47 +0100

    Create useStoreHashGivenIpfs() hook

---
+++ web-app/src/hooks/contracts/CIDMatcher/useStoreHashGivenIpfs.ts
@@ -0,0 +1,42 @@
+import {useBaseSmartContractWrite, useBaseSmartContractWriteState} from "../../utils/useBaseSmartContractWrite";
+import {useAccount, useContractWrite, useNetwork, usePrepareContractWrite, useWaitForTransaction} from "wagmi";
+import {CONTRACTS_DETAILS} from "../../../utils/constants";
+import {useEffect} from "react";
+
+export interface UseStoreHashGivenIpfsParams {
+  CIDList: string[]
+}
+
+/**
+ * Hook to store the hash of a given IPFS CID
+ */
+export const useStoreHashGivenIpfs = (params: UseStoreHashGivenIpfsParams): useBaseSmartContractWriteState<undefined> => {
+  const {completed, error, loading, result, txHash, progress, endAsyncActionError, endAsyncActionSuccess, startAsyncAction,
+    startAsyncActionWithTxHash} = useBaseSmartContractWrite<undefined>();
+  const network = useNetwork();
+  const userAccount = useAccount();
+  const prepareContractWrite = usePrepareContractWrite({
+    address: CONTRACTS_DETAILS[network.chain?.id]?.CID_MATCHER_ADDRESS,
+    abi: CONTRACTS_DETAILS[network.chain?.id]?.CID_MATCHER_ABI,
+    functionName: 'storeHashGivenIpfs',
+    enabled: true,
+    args: params.CIDList
+  });
+  const contractWrite = useContractWrite(prepareContractWrite.config);
+  const waitForTx = useWaitForTransaction({
+    hash: contractWrite.data?.hash,
+  });
+
+  useEffect(() => {
+    if (waitForTx.status === "success") endAsyncActionSuccess(undefined)
+    else if (waitForTx.status === "loading") startAsyncActionWithTxHash(contractWrite.data?.hash)
+    else if (waitForTx.status === "error") endAsyncActionError(waitForTx.error.message)
+  }, [waitForTx.status]);
+
+  useEffect(() => {
+    startAsyncAction();
+    contractWrite.write();
+  }, []);
+
+  return { completed, error, loading, result, progress, txHash };
+};
