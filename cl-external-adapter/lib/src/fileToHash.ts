02c3f53c824dffca46b36574deee6990bc3fbbd8
Federico Castelli <fc@oracleofde.fi>
2022-11-17 11:28:37 +0100

    Integrate IPFS call to check folder size

--- cl-external-adapter/lib/src/fileToHash.ts
+++ cl-external-adapter/lib/src/fileToHash.ts
@@ -5,6 +5,7 @@ import {Web3Storage} from 'web3.storage'
 // @ts-ignore
 import ab2str from "arraybuffer-to-string";
 import crypto from "crypto";
+import axios from "axios";
 
 const chainlinkHashVerifierParams = {
   CIDList: ['CIDList'],
@@ -23,31 +24,47 @@ interface HashGenerated {
   evaluatedHashList: string[]
 }
 
+const ipfsGatewayBaseUrl: string = "https://dweb.link/api/v0/ls";
+
 const generateHash = async (input: any): Promise<HashGenerated> => {
   // The Validator helps you validate the Chainlink request data
   const validator = new Validator(input, chainlinkHashVerifierParams);
   const jobRunID: string = validator.validated.id;
   const CIDList: string[] = validator.validated.data.CIDList;
   const web3Storage = new Web3Storage({ token: process.env.WEB3STORAGE_TOKEN as string });
-  const evaluatedHashList: string[] = [];
+  const evaluatedHashList: string[] = []; const CIDListResult: string[] = [];
   for (const CID of CIDList) {
     console.log(CID);
-    const res = await web3Storage.get(CID);
-    if (res === null) throw new Error("Got a null response");
-    console.log(`Got a response! [${res.status}] ${res.statusText}`);
-    if (!res.ok) throw new Error(`failed to get ${CID}`);
-    // unpack File objects from the response
-    const files = await res.files();
-    for (const file of files) {// console.log(hash);
+    const ipfsResult = await axios.get(`${ipfsGatewayBaseUrl}?arg=${CID}`);
+    if (ipfsResult.status !== 200) throw new Error(`CID ${CID} doesn't exist`);
+    const ipfsResultJson = ipfsResult.data;
+    console.log(ipfsResultJson);
+    // at the moment we just manage ipfs CIDs with just one file inside
+    if (ipfsResultJson.Objects[0].Links.length > 1) {
+      CIDListResult.push(CID);
+      evaluatedHashList.push("0x0000000000000000000000000000000000000000000000000000000000000000");
+    } else {
+      const res = await web3Storage.get(CID);
+      if (res === null) throw new Error("Got a null response");
+      console.log(`Got a response! [${res.status}] ${res.statusText}`);
+      if (!res.ok) throw new Error(`failed to get ${CID}`);
+      // unpack File objects from the response
+      const file = (await res.files())[0];
       console.log(`${file.cid} -- ${file.name} -- ${file.size}`)
       const fileBuffer = await file.arrayBuffer();
       const buf = Buffer.from(fileBuffer);
-      const hashString= crypto.createHash("sha256").update(buf).digest('hex');
+      const hashString = crypto.createHash("sha256").update(buf).digest('hex');
       console.log(hashString);
+      // we store the same hash for the folder (if exists) and the file
+      CIDListResult.push(file.cid);
       evaluatedHashList.push('0x' + hashString);
+      if (file.cid !== CID) {
+        CIDListResult.push(CID);
+        evaluatedHashList.push('0x' + hashString);
+      }
     }
   }
-  return {jobRunID, CIDList, evaluatedHashList};
+  return {jobRunID, CIDList: CIDListResult, evaluatedHashList};
 }
 
 export const lambdaHandler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
