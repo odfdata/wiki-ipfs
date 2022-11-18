710fd763003489653707935880885dc2dbf44929
Federico Castelli <fc@oracleofde.fi>
2022-11-18 17:37:11 +0100

    Fix bacalhau dockerimage error

--- bacalhau/index.ts
+++ bacalhau/index.ts
@@ -1,7 +1,6 @@
 import fs from "fs";
 import crypto, {createHash} from "crypto";
 import {ethers} from "ethers";
-import Web3 from "web3";
 
 export interface FileResult {
   CID: string,
@@ -10,7 +9,7 @@ export interface FileResult {
   verifiedOwner: boolean
 }
 
-const w3 = new Web3(process.env.MUMBAI_PROVIDER as string);
+const w3 = new ethers.providers.JsonRpcProvider(process.env.MUMBAI_PROVIDER as string);
 const walletAddressOwner: string = "0xcE753dC5D21fcDF4be528ce5f8f12898990BAca0";
 const CIDMatcherAbi = [{"inputs":[{"internalType":"string","name":"_jobId","type":"string"},{"internalType":"address","name":"_oracle","type":"address"},{"internalType":"address","name":"_LINKAddress","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"id","type":"bytes32"}],"name":"ChainlinkCancelled","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"id","type":"bytes32"}],"name":"ChainlinkFulfilled","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"id","type":"bytes32"}],"name":"ChainlinkRequested","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"string","name":"_cid","type":"string"},{"indexed":true,"internalType":"bytes32","name":"_hash","type":"bytes32"}],"name":"NewHashRecorded","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"string[]","name":"_cidList","type":"string[]"}],"name":"NewIPFSHashRequest","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"OwnershipTransferRequested","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[],"name":"ORACLE_PAYMENT","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"acceptOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_requestId","type":"bytes32"},{"internalType":"uint256","name":"_payment","type":"uint256"},{"internalType":"bytes4","name":"_callbackFunctionId","type":"bytes4"},{"internalType":"uint256","name":"_expiration","type":"uint256"}],"name":"cancelRequest","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_requestId","type":"bytes32"},{"internalType":"string[]","name":"_cidList","type":"string[]"},{"internalType":"bytes32[]","name":"_hashList","type":"bytes32[]"}],"name":"fulfillRequestIpfsToHash","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_hash","type":"bytes32"}],"name":"getCIDsFromHash","outputs":[{"internalType":"string[]","name":"CIDs","type":"string[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getChainlinkToken","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_cid","type":"string"}],"name":"getHashFromCID","outputs":[{"internalType":"bytes32","name":"hash","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_cid","type":"string"}],"name":"getOwnerOfCID","outputs":[{"internalType":"address","name":"owner","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_cid","type":"string"}],"name":"getVerificationStatus","outputs":[{"internalType":"uint256","name":"status","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_jobId","type":"string"}],"name":"setJobId","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_newLinkPayment","type":"uint256"}],"name":"setOraclePayment","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"","type":"bytes32"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"sha2ToCIDs","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string[]","name":"_cidList","type":"string[]"}],"name":"storeHashGivenIpfs","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdrawLink","outputs":[],"stateMutability":"nonpayable","type":"function"}];
 
@@ -23,7 +22,7 @@ fs.readdir("/inputs", (err, files) => {
   }
   new Promise (async (resolve, reject) => {
     const contract = await new ethers.Contract(
-        "0x553DcF1b90F0bF964219aC430C547822456EB287", CIDMatcherAbi, w3.givenProvider);
+        "0xa42761397aB5F2629d7b9e65E0c6A617f718581F", CIDMatcherAbi, w3);
     const result: FileResult[] = [];
     // for each file, generate the hash
     for (const fileName of files) {
@@ -32,13 +31,13 @@ fs.readdir("/inputs", (err, files) => {
       hashSum.update(content);
       const hash = "0x" + hashSum.digest('hex');
       // get the list of CIDs associated with the hash stored on-chain
-      const CIDList: string[] = await contract.getCIDsFromHash(hash)
+      const CIDList: string[] = await contract.getCIDsFromHash(hash);
       let CIDresult: string = "";
       let owner: string = "";
       let verifiedOwner: boolean = false;
       for (const CID of CIDList) {
         // for each CID, get the owner from the chain
-        const ownerFromChain = await contract.getOwnerOfCID([fileName]);
+        const ownerFromChain = await contract.getOwnerOfCID(CID);
         if (ownerFromChain === walletAddressOwner) {
           CIDresult = CID;
           owner = ownerFromChain;
